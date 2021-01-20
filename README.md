8d31fbfe981f4a89a5a65f9ea611956b2e1a5596
mmseng <mmseng@users.noreply.github.com>
2021-01-20 11:27:21 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Refresh-EWSRDUGroups
+# Summary
 Clears out the groups used by EWS to control remote desktop access to the labs, and sets them up for a new semester.
+
+This script does the following:
+1. Looks at all of the groups in `ad.uillinois.edu/Urbana/Engineering/UsersAndGroups/Instructional/RD User Groups`
+2. Clears out all members of these groups
+3. Creates a new sub-OU for the given `<semester identifier>`
+4. For each group in the parent OU, a new group is created in the sub-OU, named like `<group>-<semester identifier>`
+5. Each new group is added as a member of its originating group.
+
+e.g. If a semester identifier of `2020a` is given, a new sub-OU named `2020a` will be created. Taking the group `mel-1001-rdu` as an example, a new group named `mel-1001-rdu-2020a` will be created under the new `2020a` sub-OU. `mel-1001-rdu-2020a` will be added as a member of `mel-1001-rdu` (and any previous members of `mel-1001-rdu` will have been removed). Similar groups will be created for every other group in the parent OU.
+
+Existing semester-based groups will not be touched. These are intentionally left in place as a reference of RDU group memberships of past semesters until such time as EWS sees fit to bulk delete them (which is easily done using ADUC). Since these older groups will no longer be members of the parent groups (which are used in the relevant GPOs), they will have no longer have any effect.
+
+See the procedural documentation here: https://wiki.illinois.edu/wiki/display/engritprivate/EWS+remote+access+to+Windows+labs
+
+# Usage
+1. Download `Refresh-EWSRDUGroups.psm1`
+2. 
---
+++ Refresh-EWSRDUGroups.psm1
@@ -0,0 +1,83 @@
+# Script documentation home: https://github.com/engrit-illinois/Refresh-EWSRDUGroups
+# Procedure documentation home: https://wiki.illinois.edu/wiki/display/engritprivate/EWS+remote+access+to+Windows+labs
+
+function Refresh-EWSRDUGroups {
+	param(
+		[Parameter(Mandatory=$true)]
+		[ValidatePattern('^[a-zA-Z0-9]+$')]
+		[string]$SemesterIdentifier,
+		
+		[switch]$SkipRemoval
+	)
+	
+	function log($msg) {
+		$ts = Get-Date -Format "yyyy-MM-dd HH:mm:ss:ffff"
+		$msg = "[$ts] $msg"
+		Write-Host $msg
+	}
+	
+	# Record timestamp
+	$ts = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+	
+	# Define generic description for new objects
+	$descBase = "scriptomatically created on $($ts). See documentation here: https://wiki.illinois.edu/wiki/display/engritprivate/EWS+remote+access+to+Windows+labs"
+	
+	# Define OU where primary groups exist
+	$RDUOUDN = "OU=RD User Groups,OU=Instructional,OU=UsersAndGroups,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu"
+	$newOUDN = "OU=$($SemesterIdentifier),$RDUOUDN"
+	
+	# Pull all the primary groups
+	log "Pulling primary groups from `"$RDUOUDN`"..."
+	$groups = Get-ADGroup -Filter "*" -SearchBase $RDUOUDN
+	
+	# Create new sub-OU
+	log "Creating new sub-OU named `"$SemesterIdentifier`"..."
+	$desc = "This sub-OU $descBase"
+	New-ADOrganizationalUnit -Name $SemesterIdentifier -Path $RDUOUDN -Description $desc
+	
+	# Create new groups under new sub-OU
+	log "Creating new groups..."
+	foreach($group in $groups) {
+		log "    Creating new group for `"$($group.Name)`"..."
+		
+		$newName = "$($group.Name)-$SemesterIdentifier"
+		log "        New group name: `"$newName`""
+		
+		$desc = "This group $descBase"
+		
+		New-ADGroup -Name $newName -SamAccountName $newName -GroupCategory "Security" -GroupScope "Universal" -DisplayName $newName -Path $newOUDN -Description $desc
+	}
+	
+	# Remove all members of primary groups
+	if(!$SkipRemoval) {
+		log "Removing existing members from primary groups..."
+		foreach($group in $groups) {
+			log "Removing members from group `"$($group.Name)`"..."
+			
+			# Get existing members
+			$members = $group | Get-ADGroupMember
+			
+			# Remove members
+			#Remove-ADGroupMember -Identity $group.Name -Members $members
+		}
+	}
+	else {
+		log "-SkipRemoval was specified. Skipping removal of existing members from primary groups!"
+	}
+	
+	# Add new groups as members of respective primary groups
+	log "Adding new groups as members of their respective primary groups..."
+	
+	$groups = Get-ADGroup -Filter "*" -SearchBase $newOUDN
+	
+	foreach($group in $groups) {
+		# Caculate respective primary group that spawned this group
+		$primary = ($group.Name).Replace("-$SemesterIdentifier","")
+		
+		log "    Adding `"$($group.Name)`" as a member of `"$primary`"..."
+		Add-ADGroupMember -Identity $primary -Members $group.Name
+	}
+	
+	log "EOF"
+	
+}
